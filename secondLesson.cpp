0bbb1c46ccd3894ac36fe25a28fb5bbec7f4b354
MR-Salehi <92519620+MR-Salehi@users.noreply.github.com>
2021-10-15 10:24:40 +0330

    Create secondLesson.cpp
    This is section two of class

---
+++ secondLesson.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int day=5;
+    switch (day) {
+        case 5:
+            cout<<"Wednesday";
+    }
+}
