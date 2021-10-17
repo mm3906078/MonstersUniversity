a493e6631a92940bb21be31c36a365ce5c731d42
Yangholi <92567713+Yangholi@users.noreply.github.com>
2021-10-17 14:21:47 +0330

    Create Pr_MR_2.cpp

---
+++ Pr_MR_2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int metals_degree;
+	cout<<"salam alaykom";
+	cin>>metals_degree;
+	if(metals_degree>=100)
+	cout<<"steam";
+	else if(metals_degree<0)
+	cout<<"ice";
+	else
+	cout<<"water";
+	return 0;
+}
