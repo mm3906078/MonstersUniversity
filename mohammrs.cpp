93fa894d685c586eb45c769a1802c275f8fc3ee4
Yangholi <92567713+Yangholi@users.noreply.github.com>
2021-10-17 14:10:48 +0330

    Create mohammrs.cpp

---
+++ mohammrs.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"plz enter number of sen";
+	cin>>n;
+	for(int i = 0;i<n;i++)
+	{
+		cout<<"mankhoshghalbhastam"<<endl;
+	}
+	return 0;
+}
